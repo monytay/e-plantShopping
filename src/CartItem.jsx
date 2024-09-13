eb50ecd9c106ae00d85c361c61ce9239250d405c
monytay <t.majstorovic@outlook.com>
2024-09-13 10:39:49 -0400

    5 commit

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,23 +9,39 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let totalAmount = 0;
+        cart.items.forEach(item => {
+            totalAmount += item.quantity * item.cost
+        })
+        return totalAmount;
   };
 
   const handleContinueShopping = (e) => {
-   
+    e.preventDefault();
+    onContinueShopping();
   };
 
+  const handleCheckoutShopping = (e) => {
+  alert('Functionality to be added for future reference');
+  };
 
 
   const handleIncrement = (item) => {
-  };
+        dispatch(updateQuantity({name: item.name, 
+            quantity: item.quantity + 1}));
+    };
 
   const handleDecrement = (item) => {
-   
+        if (item.quantity > 1) {
+            dispatch(updateQuantity({name: item.name,
+            quantity: item.quantity - 1}));
+        }else{
+            dispatch(removeItem({name: item.name}));
+        }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem({name: item.name}));
   };
 
   // Calculate total cost based on quantity for an item
